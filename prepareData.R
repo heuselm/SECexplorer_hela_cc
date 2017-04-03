abd7f57a1e4d05df3213efffe245ea936fd77b54
heuselm <heusel@imsb.biol.ethz.ch>
2017-04-03 12:40:09 +0200

    FIX SEC-MW mapping - directly in prepareData.R

--- prepareData.R
+++ prepareData.R
@@ -1,10 +1,12 @@
 # prepare Data for visualization in shiny web app
 library(data.table)
+library(SECprofiler)
 source("tracesMethods.R")
-load("~/sonas/html/HEK293_SEC_SWATH_data_analysis/DownstreamAnalysis/SWATH_MS2_Corum_BioPlex_StringDB/StringDB/workspaceItems/string.up.09.netDist.degree1groups")
-string_subnetworks <- string.up.09.netDist.degree1groups
-rm(string.up.09.netDist.degree1groups)
-calibration_functions <- readRDS("SECMWcalibration.rda")
+
+calibration_functions = calibrateSECMW(std_weights_kDa = c(1398, 699, 300, 150, 44, 17),
+                                         std_elu_fractions = c(15.25, 24, 31, 39.275, 47.5, 53.5),
+                                         plot=TRUE,
+                                         PDF=TRUE)
 
 up <- fread("~/sonas/databases/Uniprot/uniprot-all-human9606-20161130_extended.tab")
 up[, Mass:=as.numeric(gsub(",", ".", Mass))]
@@ -40,7 +42,7 @@ trace_annotation_cum_peptides <- unique(rbind(pep_int_r1$trace_annotation[, .(pr
                                               pep_mit_r2$trace_annotation[, .(protein_id, Entry_name, Gene_names, Protein_names, protein_mw, id)],
                                               pep_mit_r3$trace_annotation[, .(protein_id, Entry_name, Gene_names, Protein_names, protein_mw, id)]))
 
-DiffCorrTable <- readRDS("../../CompareTraces/Hela_CCSEC_IvsM_DiffCorrTable_combined.rda")
+DiffCorrTable <- readRDS("../../DownstreamAnalysis/proteinCentricComparison/Hela_CCSEC_IvsM_DiffCorrTable_combined.rda")
 trace_annotation_cum <- merge(trace_annotation_cum, unique(DiffCorrTable[, .(protein_id, pearson_cor_avg,
                                                                              pearson_cor_avg_rank,
                                                                              difference_normalized_avg,
