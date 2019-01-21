ce7ae38917acadcbdf285885e8151023cb5bb945
Max Frank <max.florian.frank@gmail.com>
2019-01-21 21:52:47 +0100

    Add authentication info

--- deploy.R
+++ deploy.R
@@ -1,3 +1,9 @@
+### Deployment to shinyapps.io
 library(rsconnect)
+### Before deploying make sure that your computer is authenticated
+### You find the secret and the token on shinyapps.io after logging in
+## rsconnect::setAccountInfo(name='sec-explorer',
+## token='TOKEN',
+## secret='SECRET')
 # This path needs to be changed to the local folder containing the app
 rsconnect::deployApp('~/projects/HeLa_Shiny_App/')
